ff2e9162651a39d0be207a8bef383a9dd3aa6aa2
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-08-23 11:22:17 +0800

    Add acentives discount inquiry

---
+++ wp-content/themes/ali-corporate/ajax/acentives_discount_inquiry.php
@@ -0,0 +1,24 @@
+<?php
+
+include("jp_lib/jp_lib.php");
+
+$item['table'] = "emails_acentives_discount";
+
+$_POST["date_sent"] = date("Y-m-d H:i:s");
+
+$item['data'] = $_POST;
+if(jp_add($item)){
+  $subject = "ALI CORPORATE SALES [ACENTIVES DISCOUNT INQUIRY]";
+  $message = "Am I qualified for Acentives Discount? \n
+  Employer Name: ".$_POST['employer']." \n
+  Name: ".$_POST['name']." \n
+  Email: ".$_POST['email']." \n
+  Years of service: ".$_POST['service_years']." \n
+  Name of Project/Unit: ".$_POST['project']." \n
+  Employee Type: ".$_POST['employee_type']." \n
+  ";
+  $headers = "From: noreply@betaprojex.com \r\n";
+  $to = "lsalamante@myoptimind.com";
+
+  echo (mail($to,$subject,$message,$headers)) ? 1 : 0 ;
+}
--- wp-content/themes/ali-corporate/template-inquire-now.php
+++ wp-content/themes/ali-corporate/template-inquire-now.php
@@ -9,123 +9,104 @@ while(have_posts()): the_post();
     <h3>Inquire Now</h3>
     <aside class="tab">
       <ul>
-        <li class="active"><a href="inquire-now.html">Am I qualified for Acentives Discount?</a></li>
-        <li><a href="send-inquiry.html">Send Inquiry</a></li>
+        <li ><a href="inquire-now.html">Am I qualified for Acentives Discount?</a></li>
+        <li class="active"><a href="send-inquiry.html">Send Inquiry</a></li>
 
       </ul>
     </aside>
-    <div class="forms-main">
-      <p>Fill out the form below:</p>
-      <ul>
-        <li>
-          <label>Your Employer</label>
-          <select>
-            <option>List of Employer</option>
-            <option>AC Energy and Infrastructure Group (and subsidiaries)</option>
-            <option>Accendo Commercial Corp</option>
-            <option>Affinity Express</option>
-            <option>Affordable Private Education Centers (APEC)</option>
-            <option>Automobile Central Enterprise, Inc. (Volkswagen Philippines)</option>
-            <option>Avida Land Corp.</option>
-            <option>Ayala Aviation Corporation</option>
-            <option>Ayala Corporation</option>
-            <option>Ayala Education</option>
-            <option>Ayala Foundation, Inc.</option>
-            <option>Ayala Hotels, Inc.</option>
-            <option>Ayala Land Commercial REIT, Inc.</option>
-            <option>Ayala Land International Sales, Inc.</option>
-            <option>Ayala Land Malls, Inc.</option>
-            <option>Ayala Land Sales, Inc.</option>
-            <option>Ayala Land, Inc.</option>
-            <option>Ayala Property Management Corporation</option>
-            <option>Ayala Theatres Management, Inc.</option>
-            <option>Bank of the Philippine Islands</option>
-            <option>BellaVita Land Corp.</option>
-            <option>BPI Philam Life Assurance Corp.</option>
-            <option>BPI/MS Insurance Corporation</option>
-            <option>CDO Gateway Corp.</option>
-            <option>Cebu City Marriott Hotel</option>
-            <option>Cebu Holdings, Inc.</option>
-            <option>City Sports Club</option>
-            <option>Fort Bonifacio Development Corp.</option>
-            <option>Generika Drugstore</option>
-            <option>Globe Telecom, Inc.</option>
-            <option>Holiday Inn &amp; Suites Makati</option>
-            <option>Honda Cars Cebu, Inc</option>
-            <option>Honda Cars Makati, Inc.</option>
-            <option>HRMall, Inc.</option>
-            <option>Integrated Microelectronics, Inc.</option>
-            <option>Isuzu Automotive Dealership, Inc.</option>
-            <option>Isuzu Cebu, Inc. / Isuzu Iloilo Corporation</option>
-            <option>Laguna Technopark, Inc.</option>
-            <option>Leisure and Allied Industries Philippines, Inc.</option>
-            <option>Light Rail Manila Corporation</option>
-            <option>Makati Development Corporation</option>
-            <option>Manila Water Company, Inc.</option>
-            <option>Mercado General Hospital Inc. Group of Companies (Qualimed)</option>
-            <option>North Triangle Depot Commercial Corporation</option>
-            <option>Northbeacon Commercial Corporation</option>
-            <option>Phil. Energy</option>
-            <option>Primavera Towncentre Inc</option>
-            <option>Raffles &amp; Fairmont Hotel</option>
-            <option>Sonoma Services</option>
-            <option>South Luzon Thermal Energy Corp.</option>
-            <option>Station Square East Commercial Corporation</option>
-            <option>Subic Bay Town Center</option>
-            <option>Ten Knots Development Corporation- El Nido Resorts</option>
-            <option>University of Nueva Caceres (UNC)</option>
-            <option>Varejo Corp.</option>
-            <option>Zalora</option>
-          </select>
-        </li>
-        <li>
-          <label>Your Name</label>
-          <input type="text" name="name">
-        </li>
-        <li>
-          <label>Your Email</label>
-          <input type="email" name="email">
-        </li>
-        <li>
-          <label>Years of Service</label>
-          <select>
-            <option>Select years of service</option>
-            <option>1 - 2 years</option>
-            <option>3 - 4 years</option>
-            <option>5 - 6 years</option>
-          </select>
-        </li>
-        <li>
-          <label>Name of Project/Unit</label>
-          <select>
-            <option>Select Brand</option>
-            <option>Ayala Land Premier</option>
-            <option>Alveo Land</option>
-            <option>Avida Land</option>
-            <option>Amaia Land</option>
-            <option>Bellavita</option>
-          </select>
-        </li>
-        <li>
-          <label class="hide">&nbsp;</label>
-          <ul>
-            <li><input type="radio" name=""><label>Regular Employee</label></li>
-            <li><input type="radio" name=""><label>Contractual Employee</label></li>
-          </ul>
+    <form method="post" id="_form">
 
-        </li>
-        <li>
+      <div class="forms-main">
+        <p>Fill out the form below:</p>
+        <ul>
+          <li>
+            <label>Your Employer</label>
+            <select name="employer">
 
-          <input type="submit" name="">
-        </li>
-      </ul>
-    </div>
-  </article>
+              <?php
+              $employers = get_field("employers_list", 884);
+              foreach ($employers as $emp) { ?>
+                <option <?php echo ($emp["employee_name"] == $_SESSION['employer_name']) ? 'selected' :'' ; ?>>
+                  <?php echo $emp["employee_name"]; ?></option>
+                  <?php
+                }
+                ?>
 
-  <div class="other-links">
+              </select>
+            </li>
+            <li>
+              <label>Your Name</label>
+              <input type="text" name="name">
+            </li>
+            <li>
+              <label>Your Email</label>
+              <input type="email" name="email">
+            </li>
+            <li>
+              <label>Years of Service</label>
+              <select name="service_years">
+                <option>Select years of service</option>
+                <option>1 - 2 years</option>
+                <option>3 - 4 years</option>
+                <option>5 - 6 years</option>
+              </select>
+            </li>
+            <li>
+              <label>Name of Project/Unit</label>
+              <select name="project">
+                <option>Select Brand</option>
+                <option>Ayala Land Premier</option>
+                <option>Alveo Land</option>
+                <option>Avida Land</option>
+                <option>Amaia Land</option>
+                <option>Bellavita</option>
+              </select>
+            </li>
+            <li>
+              <label class="hide">&nbsp;</label>
+              <ul>
+                <li><label><input type="radio" name="employee_type" value="Regular Employee">Regular Employee</label></li>
+                <li><label><input type="radio" name="employee_type" value="Contractual Employee">Contractual Employee</label></li>
+              </ul>
+            </li>
+            <li>
+              <input type="submit">
+            </li>
+          </form>
+        </ul>
+      </div>
+    </article>
+
+    <div class="other-links">
       <?php displaySideNav('generic'); # Find this in the header ?>
-  </div>
-</section>
-<?php
+    </div>
+  </section>
+  <?php
 endwhile;
 get_footer();
+?>
+
+<script>
+$(document).ready(function() {
+  $("#_form").on('submit', function(e){
+    e.preventDefault();
+
+    $.ajax({
+      url:"<?php echo get_template_directory_uri(); ?>/ajax/acentives_discount_inquiry.php",
+      type: "POST",
+      data: $(this).serialize(),
+      success:function(data){
+
+        if(data == 1){
+          window.location.href = '<?php echo get_permalink(1141); ?>?type=acentives_discount_inquiry';
+        }
+      }, // success end
+      error: function(e){
+        console.log(e);
+      }
+    }); // ajax end
+
+
+  });
+});
+</script>
