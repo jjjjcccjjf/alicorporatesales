6b902bbea40843e0169cb4ea62f6bc7c5c45deb7
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-08-29 11:10:38 +0800

    Add do you agree

--- wp-content/themes/ali-corporate/template-arc.php
+++ wp-content/themes/ali-corporate/template-arc.php
@@ -8,51 +8,55 @@ while(have_posts()): the_post();
   <article class="forms">
     <h3 style="color:#000">ARC Inquire Now</h3>
     <!-- <aside class="tab">
-      <ul>
-        <li ><a href="<?php #echo get_permalink(925); ?>">Am I qualified for Acentives Discount?</a></li>
-        <li class="current"><a href="<?php# echo get_permalink(943); ?>">Send Inquiry</a></li>
-      </ul>
-    </aside> -->
-    <form id="_form">
-    <div class="forms-main">
-      <p>Fill out the form below:</p>
-      <ul>
-        <li>
-          <label>Your Name</label>
-          <input type="text" name="name" required>
-        </li>
-        <li>
-          <label>Your Email</label>
-          <input type="email" name="email" required>
-        </li>
-        <li>
-          <label>Property Purchased</label>
-          <select name="property_purchased" required>
-            <option disabled >Select Brand</option>
-            <?php
-            $field_key = "field_59914863f7455"; # Find this in ACF itself under `Screen Options`
-            $field = get_field_object($field_key);
-            if($field){
-              foreach( $field['choices'] as $choice ) { ?>
-                <option <?php echo isset($_GET['b']) && $_GET['b'] == $choice ? "selected" : ""; ?>><?php echo $choice; ?></option>
-                <?php
-              }
-            }
-            ?>
-          </select>
-        </li>
-        <li>
-          <label>Price</label>
-          <input type="text" name="price">
-        </li>
-
-        <li>
-
-          <input type="submit">
-        </form>
+    <ul>
+    <li ><a href="<?php #echo get_permalink(925); ?>">Am I qualified for Acentives Discount?</a></li>
+    <li class="current"><a href="<?php# echo get_permalink(943); ?>">Send Inquiry</a></li>
+  </ul>
+</aside> -->
+<form id="_form">
+  <div class="forms-main">
+    <p>Fill out the form below:</p>
+    <ul>
+      <li>
+        <label>Your Name</label>
+        <input type="text" name="name" required>
       </li>
-    </ul>
-  </div>
+      <li>
+        <label>Your Email</label>
+        <input type="email" name="email" required>
+      </li>
+      <li>
+        <label>Property Purchased</label>
+        <select name="property_purchased" required>
+          <option disabled >Select Brand</option>
+          <?php
+          $field_key = "field_59914863f7455"; # Find this in ACF itself under `Screen Options`
+          $field = get_field_object($field_key);
+          if($field){
+            foreach( $field['choices'] as $choice ) { ?>
+              <option <?php echo isset($_GET['b']) && $_GET['b'] == $choice ? "selected" : ""; ?>><?php echo $choice; ?></option>
+              <?php
+            }
+          }
+          ?>
+        </select>
+      </li>
+      <li>
+        <label>Price</label>
+        <input type="text" name="price">
+      </li>
+      <li>
+        <p>
+          <input type="checkbox" value="1" id="agree_terms"> I agree to the
+          <a href="<?php echo get_permalink(917) ?>">Privacy Policy</a> and <a href="<?php echo get_permalink(907) ?>">Terms and Conditions</a>
+        </p>
+      </li>
+      <li>
+        <input type="submit">
+      </form>
+    </li>
+  </ul>
+</div>
 
 </article>
 
@@ -70,21 +74,24 @@ $(document).ready(function() {
   $("#_form").on('submit', function(e){
     e.preventDefault();
 
-    $.ajax({
-      url:"<?php echo get_template_directory_uri(); ?>/ajax/arc_inquiry.php",
-      type: "POST",
-      data: $(this).serialize(),
-      success:function(data){
+    if($('#agree_terms:checked').length > 0){
+      $.ajax({
+        url:"<?php echo get_template_directory_uri(); ?>/ajax/arc_inquiry.php",
+        type: "POST",
+        data: $(this).serialize(),
+        success:function(data){
 
-        if(data == 1){
-          window.location.href = '<?php echo get_permalink(1141); ?>?type=arc_inquiry';
+          if(data == 1){
+            window.location.href = '<?php echo get_permalink(1141); ?>?type=arc_inquiry';
+          }
+        }, // success end
+        error: function(e){
+          console.log(e);
         }
-      }, // success end
-      error: function(e){
-        console.log(e);
-      }
-    }); // ajax end
-
+      }); // ajax end
+    }else{
+      alert('You must agree to the privacy policy and terms and conditions to proceed.');
+    }
 
   });
 });
