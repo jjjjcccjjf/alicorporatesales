b35b68d0c8b8324890d899a9b78e5fd55a67332d
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-09-11 15:49:53 +0800

    Change property finder logic

--- wp-content/themes/ali-corporate/template-projects.php
+++ wp-content/themes/ali-corporate/template-projects.php
@@ -17,7 +17,7 @@ if($_GET['property_type'] != null){
     'key' => 'project_type',
     'value' => @$_GET['property_type'],
     'compare' => 'LIKE' # Minor BUG: Equal doesn't work. So we use like since this is checkbox :/
-    );
+  );
 }else{
   $property_type_filter = [];
 }
@@ -27,7 +27,7 @@ if($_GET['property_location'] != null){
     'key' => 'location',
     'value' => $location_ID, # should be the ID of locaton
     'compare' => '='
-    );
+  );
 }else{
   $location_filter = [];
 }
@@ -37,7 +37,7 @@ if($_GET['brand'] != null){
     'key' => 'brand',
     'value' => @$_GET['brand'],
     'compare' => 'LIKE'
-    );
+  );
 }else{
   $brand_filter = [];
 }
@@ -52,14 +52,14 @@ if($_GET['price_range'] != null){
     'value' => array($lower, $higher),
     'compare' => 'BETWEEN',
     'type' => 'NUMERIC'
-    );
+  );
 
   $price_range_filter_max = array(
     'key' => 'max_price',
     'value' => array($lower, $higher),
     'compare' => 'BETWEEN',
     'type' => 'NUMERIC'
-    );
+  );
 
   if($higher == 'ABOVE'){
     $price_range_filter_max = array(
@@ -67,14 +67,14 @@ if($_GET['price_range'] != null){
       'value' => 30000001, # HACK: Harharhar
       'compare' => '>=',
       'type' => 'NUMERIC'
-      );
+    );
   }
 
   $price_range_filter = array(
     'relation' => 'OR',
     $price_range_filter_min,
     $price_range_filter_max,
-    );
+  );
 
 }else{
   $price_range_filter = [];
@@ -91,6 +91,56 @@ while(have_posts()): the_post();
 
 include_once('function-pagination.php');
 $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+
+
+############# THIS IS THE BLOCK FOR GETTING THE PROJECT IDS ONLY
+# Project ids array
+$project_ids_array = [];
+
+$args = array('post_type' => 'project', 'posts_per_page' => -1, 'order' => 'ASC', 'orderby' => 'title');
+$args['meta_query'] = array( 'relation' => 'AND' );
+if($brand_filter !== []){
+  $args['meta_query'][] = $brand_filter;
+}
+if($location_filter !== []){
+  $args['meta_query'][] = $location_filter;
+}
+if($property_type_filter !== []){
+  $args['meta_query'][] = $property_type_filter;
+}
+$the_query = new WP_Query($args);
+// var_dump($the_query); die();
+if ( $the_query->have_posts() ) {  while ( $the_query->have_posts() ): $the_query->the_post();
+
+  //
+  // BLOCK FOR fetching specific IDS per price
+  //
+  $min_price = get_field('min_price');
+  $max_price = get_field('max_price');
+
+  if($lower != null && $higher != null){
+    # FOr the lower value
+    $min_between = (($lower >= $min_price) && ($lower <= $max_price));
+    # Handler for string 'above'. Yeah poor coding shit, i know. But this is the best *for now*
+    if($higher == 'ABOVE'){
+      $max_between = ((999999999999 >= $min_price) && (30000001 <=  $max_price));
+    }else{
+      $max_between = (($higher >= $min_price) && ($higher <=  $max_price));
+    }
+    $show_projects = ($min_between || $max_between);
+  }else{
+    $show_projects = true;
+  }
+
+  if($show_projects){
+    # Filter the locatoin  (-________________-)
+    // if($location_w == get_field('location')->post_title){
+    $ids_of_projects[] = get_the_ID();
+    // }
+  }
+
+endwhile; wp_reset_postdata(); } else { }
+############# / THIS IS THE BLOCK FOR GETTING THE PROJECT IDS ONLY
 
 ?>
 
@@ -111,119 +161,114 @@ $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
       <ul>
 
         <?php
-        $args = array('post_type' => 'project', 'posts_per_page' => 12, 'paged' => $paged, 'order' => 'ASC', 'orderby' => 'title');
-        $args['meta_query'] = array( 'relation' => 'AND' );
-        if($brand_filter !== []){
-          $args['meta_query'][] = $brand_filter;
-        }
-        if($location_filter !== []){
-          $args['meta_query'][] = $location_filter;
-        }
-        if($property_type_filter !== []){
-          $args['meta_query'][] = $property_type_filter;
-        }
-        if($price_range_filter !== []){
-          $args['meta_query'][] = $price_range_filter;
+
+        # Fuck wp devs!!!
+        # https://stackoverflow.com/questions/23247671/wordpress-query-showing-post-in-empty-post-in-array
+        if(empty($ids_of_projects)) {
+          $ids_of_projects = ['issue#28099'];
         }
 
-        $the_query = new WP_Query($args);
-        if ( $the_query->have_posts() ) {  while ( $the_query->have_posts() ): $the_query->the_post(); ?>
+        $real_args = array('post_type' => 'project', 'posts_per_page' => 12,
+        'paged' => $paged, 'order' => 'ASC',
+        'orderby' => 'title', 'post__in' => $ids_of_projects);
+        $real_query = new WP_Query($real_args);
+        if ( $real_query->have_posts() ) {  while ( $real_query->have_posts() ): $real_query->the_post(); ?>
 
-        <li>
-          <a href="#projdesc<?php echo get_the_ID(); ?>" class="desc">
-            <figure>
-              <img src="<?php echo checkPhoto(get_the_post_thumbnail_url()); # Find in header.php. Checks for blank photo, gives placeholder photo if true ?>">
-            </figure>
-            <figcaption>
-              <h4><?php the_title();?></h4>
-              <span class="price">
-                <h6>Price range</h6>
-                <h5><?php echo formatPrice(get_field('min_price')); ?> -
-                  <?php echo formatPrice(get_field('max_price')); ?></h5>
-              </span>
-              <span class="type-location">
-                <p><?php echo implode(", ", get_field('project_type')); ?></p>
-                <p><?php echo get_field('location')->post_title; ?></p>
-              </span>
-            </figcaption>
-          </a>
-        </li>
-
-        <!-- Project Details -->
-        <div id="projdesc<?php echo get_the_ID(); ?>" class="white-popup mfp-hide projdesc">
-          <h3><?php the_title();?></h3>
-          <div>
-            <figure>
-              <img src="<?php echo checkPhoto(get_field('details_photo')); # Find in header.php. Checks for blank photo, gives placeholder photo if true?>">
-            </figure>
-            <article>
-              <ul>
-                <li>
-                  <h6>Price:</h6>
-                  <p><?php echo formatPrice(get_field('min_price')); ?> -
-                    <?php echo formatPrice(get_field('max_price')); ?></p>
-                  </li>
-                  <li>
-                    <h6>Location:</h6>
-                    <p><?php echo get_field('location')->post_title; ?></p>
-                  </li>
-                  <li>
-                    <h6>Type:</h6>
+          <li>
+            <a href="#projdesc<?php echo get_the_ID(); ?>" class="desc">
+              <figure>
+                <img src="<?php echo checkPhoto(get_the_post_thumbnail_url()); # Find in header.php. Checks for blank photo, gives placeholder photo if true ?>">
+              </figure>
+              <figcaption>
+                <h4><?php the_title();?></h4>
+                <span class="price">
+                  <h6>Price range</h6>
+                  <h5><?php echo formatPrice(get_field('min_price')); ?> -
+                    <?php echo formatPrice(get_field('max_price')); ?></h5>
+                  </span>
+                  <span class="type-location">
                     <p><?php echo implode(", ", get_field('project_type')); ?></p>
-                  </li>
-                  <li>
-                    <h6>Size:</h6>
-                    <p><?php the_field('size'); ?></p>
-                  </li>
-                  <!-- <li ><a href="<?php # the_field('external_link'); ?>">Virtual Tour</a></li> -->
-                  <!-- TODO: !!! -->
-                </ul>
+                    <p><?php echo get_field('location')->post_title; ?></p>
+                  </span>
+                </figcaption>
+              </a>
+            </li>
 
-              </article>
-              <aside>
-                <?php
-                $project_title = get_the_title();
-                $project_brand = str_replace(" ", "_", get_field("brand"));
-                $virtual_tour = get_field('virtual_tour_link');
-                ?>
-                <ul>
-                  <li><a href="<?php echo get_permalink(925) ?>?t=<?php echo $project_title; ?>&b=<?php echo $project_brand; ?>">Inquire Now</a></li>
-                  <li><a href="<?php echo get_permalink(996) ?>?t=<?php echo $project_title; ?>&b=<?php echo $project_brand; ?>">Refer Now</a></li>
-                  <?php if($virtual_tour != ""): ?>
-                  <li><a href="<?php echo $virtual_tour; ?>" target="_blank">Virtual Tour</a></li>
-                  <?php endif; ?>
-                </ul>
-              </aside>
-            </div>
-          </div>
-          <!-- Project Details -->
-        <?php endwhile; wp_reset_postdata(); } else { ?>
+            <!-- Project Details -->
+            <div id="projdesc<?php echo get_the_ID(); ?>" class="white-popup mfp-hide projdesc">
+              <h3><?php the_title();?></h3>
+              <div>
+                <figure>
+                  <img src="<?php echo checkPhoto(get_field('details_photo')); # Find in header.php. Checks for blank photo, gives placeholder photo if true?>">
+                </figure>
+                <article>
+                  <ul>
+                    <li>
+                      <h6>Price:</h6>
+                      <p><?php echo formatPrice(get_field('min_price')); ?> -
+                        <?php echo formatPrice(get_field('max_price')); ?></p>
+                      </li>
+                      <li>
+                        <h6>Location:</h6>
+                        <p><?php echo get_field('location')->post_title; ?></p>
+                      </li>
+                      <li>
+                        <h6>Type:</h6>
+                        <p><?php echo implode(", ", get_field('project_type')); ?></p>
+                      </li>
+                      <li>
+                        <h6>Size:</h6>
+                        <p><?php the_field('size'); ?></p>
+                      </li>
+                      <!-- <li ><a href="<?php # the_field('external_link'); ?>">Virtual Tour</a></li> -->
+                      <!-- TODO: !!! -->
+                    </ul>
 
-        <li>
-          <div style="height:400px; width: 300px; color:#000;">
-            No active projects at this time.
-          </div>
-        </li>
+                  </article>
+                  <aside>
+                    <?php
+                    $project_title = get_the_title();
+                    $project_brand = str_replace(" ", "_", get_field("brand"));
+                    $virtual_tour = get_field('virtual_tour_link');
+                    ?>
+                    <ul>
+                      <li><a href="<?php echo get_permalink(925) ?>?t=<?php echo $project_title; ?>&b=<?php echo $project_brand; ?>">Inquire Now</a></li>
+                      <li><a href="<?php echo get_permalink(996) ?>?t=<?php echo $project_title; ?>&b=<?php echo $project_brand; ?>">Refer Now</a></li>
+                      <?php if($virtual_tour != ""): ?>
+                        <li><a href="<?php echo $virtual_tour; ?>" target="_blank">Virtual Tour</a></li>
+                      <?php endif; ?>
+                    </ul>
+                  </aside>
+                </div>
+              </div>
+              <!-- Project Details -->
+            <?php endwhile; wp_reset_postdata(); } else { ?>
 
-        <?php } ?>
-      </ul>
-    </div>
-    <section>
-      <ul>
-        <?php
-        if (function_exists(custom_pagination)) {
-          custom_pagination($the_query->max_num_pages,"",$paged);
-        }
-        ?>
-      </ul>
+              <li>
+                <div style="height:400px; width: 300px; color:#000;">
+                  No active projects at this time.
+                </div>
+              </li>
+
+            <?php } ?>
+          </ul>
+        </div>
+        <section>
+          <ul>
+            <?php
+            if (function_exists(custom_pagination)) {
+              custom_pagination($real_query->max_num_pages,"",$paged);
+            }
+            ?>
+          </ul>
+        </section>
+      </article>
+
+      <div class="other-links">
+        <?php displaySideNav($_SESSION['employer_type']); # Find this in header.php ?>
+      </div>
     </section>
-  </article>
 
-  <div class="other-links">
-    <?php displaySideNav($_SESSION['employer_type']); # Find this in header.php ?>
-  </div>
-</section>
-
-<?php
-endwhile;
-get_footer();
+    <?php
+  endwhile;
+  get_footer();
